5b9593b4cd750b463e3b0395e54d0d9c142ae280
Rachel Liu <rachel98rl@hotmail.com>
2017-10-22 06:48:14 -0400

    Add files via upload

---
+++ face_detection.py
@@ -0,0 +1,50 @@
+########### Python 3.6 #############
+import http.client, urllib.request, urllib.parse, urllib.error, base64, requests, json
+
+###############################################
+#### Update or verify the following values. ###
+###############################################
+
+# Replace the subscription_key string value with your valid subscription key.
+subscription_key = 'b24d81ba7afc49a6b05a807edf4e1bf8'
+
+# Replace or verify the region.
+#
+# You must use the same region in your REST API call as you used to obtain your subscription keys.
+# For example, if you obtained your subscription keys from the westus region, replace
+# "westcentralus" in the URI below with "westus".
+#
+# NOTE: Free trial subscription keys are generated in the westcentralus region, so if you are using
+# a free trial subscription key, you should not need to change this region.
+
+def face_detection(img):
+    uri_base = 'https://westcentralus.api.cognitive.microsoft.com'
+
+    # Request headers.
+    headers = {
+        'Content-Type': 'application/json',
+        'Ocp-Apim-Subscription-Key': subscription_key,
+    }
+
+    # Request parameters.
+    params = {
+        'returnFaceId': 'true',
+        'returnFaceLandmarks': 'false',
+        'returnFaceAttributes': 'age,gender,headPose,smile,facialHair,glasses,emotion,hair,makeup,occlusion,accessories,blur,exposure,noise',
+    }
+
+    # Body. The URL of a JPEG image to analyze.
+    body = {'url': img}
+
+    try:
+        # Execute the REST API call and get the response.
+        response = requests.request('POST', uri_base + '/face/v1.0/detect', json=body, data=None, headers=headers, params=params)
+
+
+        parsed = json.loads(response.text)
+        return json.dumps(parsed, sort_keys=True, indent=2)
+
+    except Exception as e:
+        print('Error:')
+        print(e)
+
---
+++ test.py
@@ -0,0 +1,32 @@
+from face_detection import face_detection as face
+from handwriting_to_text import handwriting_to_text as handwriting
+import os
+import json
+
+# img_list is a list of urls
+def photo_analyse (img_list):
+    img_path_list = []
+    filtered_img_list = []
+    for img in img_list:
+        detect = face(img)
+        if detect is not []:
+            img_path_list.append(str(img))
+            filtered_img_list.append(img)
+    analyzed_imgs = []
+    text = []
+    for i in range(len(filtered_img_list)):
+        filtered_img_list[i] = handwriting(filtered_img_list[i])
+        lines = json.loads(filtered_img_list[i])['recognitionResult']['lines']
+        text.append('')
+        for line in lines:
+            text[i] += line['text'] + '\n'
+    for i in range(len(text)):
+        if text[i] not in text[i+1:]:
+            analyzed_imgs.append(img_path_list[i])
+    print(analyzed_imgs)
+    return analyzed_imgs
+
+test_img_list = ["http://joshldavis.com/img/handwriting/dijkstras.png", \
+                 "https://media.npr.org/assets/img/2016/04/17/handwritten-note_wide-941ca37f3638dca912c8b9efda05ee9fefbf3147.jpg?s=1400",\
+                 "http://joshldavis.com/img/handwriting/dijkstras.png"]
+photo_analyse(test_img_list)
