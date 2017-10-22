52ccbc0d0ca1e17c3ff00eb74f4cf96abc3d8ccf
ezhxzeng <zhixuan.zeng@gmail.com>
2017-10-22 06:51:23 -0400

    uploaded onedrive api implementation

---
+++ MsGraph_OneNoteApiSampleNodejs-master/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    // App's API information - replace with your own values
+    clientId: process.env.CLIENT_ID || '23b61308-fe2b-4a01-ab1d-062787ffb5fb',
+    clientSecret: process.env.CLIENT_SECRET || 'FMBwmjcRJMCdAZesEqSjtfd',
+    redirectUrl: process.env.REDIRECT_URI || 'http://localhost:3000/callback'
+};
